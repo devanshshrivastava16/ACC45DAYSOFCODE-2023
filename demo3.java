a574727705a7eeba67c0ecbb0dc86def0779716a
Devansh Shrivastava <145153202+devanshshrivastava16@users.noreply.github.com>
2023-10-04 20:03:53 +0530

    Day 16

---
+++ demo3.java
@@ -0,0 +1,62 @@
+import java.util.*;
+class demo3
+{
+    String s;
+    void get()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Senstance");
+        s=sc.nextLine();
+    }
+    void show()
+    {
+        int i,l;
+        char a,b;
+        String w;
+        StringTokenizer st=new StringTokenizer(s);
+        int c=st.countTokens();
+        for(i=0;i<=c;i++)
+        {
+            w=st.nextToken();
+            l=w.length();
+            a=w.charAt(0);
+            b=w.charAt(l-1);
+            
+            
+            if(Character.isUpperCase(a)&&Character.isLowerCase(b))
+            {
+                System.out.println(w);
+            }
+        }
+    }
+    void check()
+    {
+        String w,w1,rev="";
+        int i,l,j;
+        StringTokenizer st=new StringTokenizer(s);
+        int c=st.countTokens();
+        for(i=0;i<=c;i++)
+        {
+            
+            w=st.nextToken();
+            w1=w.toLowerCase();
+            l=w.length();
+            for( j=l-1;j>=0;j--)
+            {
+               
+               rev=rev+w1.charAt(j);
+            }
+            if (rev.equalsIgnoreCase(w1))
+            {
+               System.out.println(w+" is pallamdrome");
+            }
+        }
+    }
+    public static void main(String args[])
+    {
+        demo3 ob=new demo3();
+        ob.get();
+        ob.show();
+        ob.check();
+    }
+}
